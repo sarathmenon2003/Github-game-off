faf33b825fc5a92473c1d3957e821dbe5238ae7a
sarathmenon2003 <73239696+sarathmenon2003@users.noreply.github.com>
2020-11-09 00:09:10 +0400

    change_2

---
+++ Game jam (GH)/Assets/playermovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 10f;
+    public Animator animator;
+    public float jumpForce;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 dir = new Vector2(x, y);
+        Walk(dir);
+        animator.SetFloat("speed", Mathf.Abs (x));
+    
+    
+    }
+    private void Walk(Vector2 dir)
+	{
+        rb.velocity = (new Vector2(dir.x * speed, rb.velocity.y));
+            
+	}
+    void Update()
+	{
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+        if(isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+		{
+            rb.velocity = Vector2.up * jumpForce;
+		}
+	}
+    
+}
