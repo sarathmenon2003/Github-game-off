557d658c009ea32665eb2cb606f1df2212a65dd2
sarathmenon2003 <73239696+sarathmenon2003@users.noreply.github.com>
2020-11-15 09:43:13 +0400

    change_01

--- Game jam (GH)/Assets/scripts/playermovement.cs
+++ Game jam (GH)/Assets/scripts/playermovement.cs
@@ -4,80 +4,22 @@ using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    public float speed = 10f;
-    public Animator animator;
-    public float jumpForce;
+	public CharacterController2D controller;
+	float horizontalMove = 0f;
+	public float runSpeed;
+	bool jump = false;
 
-    private bool isGrounded;
-    public Transform feetPos;
-    public float checkRadius;
-    public LayerMask whatIsGround;
-    private int extraJumps;
-    public int extraJumpsValue;
-    bool facingRight = true;
-    // Start is called before the first frame update
-    void Start()
-    {
-        extraJumps = extraJumpsValue;
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-        Vector2 dir = new Vector2(x, y);
-        Walk(dir);
-        animator.SetFloat("speed", Mathf.Abs(x));
-        
-        if (x < 0 && facingRight)
-        {
-            flip();
-        }
-        else if (x > 0 && !facingRight)
-		{
-            flip();
-		}
-       
-
-
-        animator.SetBool("isJumping", !isGrounded);
-        animator.SetFloat("yVel", rb.velocity.y);
-    }
-    private void Walk(Vector2 dir)
-    {
-        rb.velocity = (new Vector2(dir.x * speed, rb.velocity.y));
-
-    }
-    void Update()
-    {
-        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
-
-        if (isGrounded == true)
-        {
-            extraJumps = extraJumpsValue;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
-        {
-            animator.SetBool("isJumping", true);
-            rb.velocity = Vector2.up * jumpForce;
-            extraJumps--;
-        }
-        else if (isGrounded == true && Input.GetKeyDown(KeyCode.Space) && extraJumps == 0)
-        {
-            animator.SetBool("isJumping", true);
-            rb.velocity = Vector2.up * jumpForce;
-        }
-
-    }
-    void flip()
+	void Update()
 	{
-
-        facingRight = !facingRight;
-        transform.Rotate(0f, 180f, 0f);
+		horizontalMove = Input.GetAxisRaw("Horizontal")* runSpeed;
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
 	}
-
+	void FixedUpdate()
+	{
+		controller.Move(horizontalMove* Time.fixedDeltaTime, false, jump) ;
+		jump = false;
+	}
 }
